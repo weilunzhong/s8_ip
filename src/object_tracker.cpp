60e78177681ab1533730ab44a4125419ba343a30
Unnar3 <unnar@kth.se>
2014-10-16 00:13:06 +0200

    fixed the syntax

--- src/object_tracker.cpp
+++ src/object_tracker.cpp
@@ -7,12 +7,12 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <message_filters/synchronizer.h>
 #include <signal.h>
-#include <cmath>
 
 using namespace cv;
 using namespace std;
 
-#define HZ 10
+#define HZ 	10
+
 static const std::string OPENCV_WINDOW = "Image window";
 static const std::string OPENCV_WINDOW_DEPTH = "Depth Image Windows";
 static const std::string OPENCV_WINDOW_CONTROL = "Control Window";
@@ -24,7 +24,7 @@ class ObjectTracker
 	image_transport::Subscriber image_sub_;
 	image_transport::Subscriber image_depth_sub_;
 	image_transport::Publisher image_pub_;
-	int iLowH, iHighH, iLowS, iHighS, iLowV, iHighV, iErode, iUpperRange;
+	int iLowH, iHighH, iLowS, iHighS, iLowV, iHighV, iErode, StepSize;
 	bool isInitializedDepth, isInitializedColor;
 	cv_bridge::CvImagePtr cv_depth_ptr;
 	cv_bridge::CvImagePtr cv_ptr;
@@ -39,38 +39,24 @@ public:
 		image_depth_sub_ = it_.subscribe("/camera/depth/image_raw", 1, 
 		  &ObjectTracker::imageDepthCb, this);
 		image_pub_ = it_.advertise("/image_converter/output_video", 1);
-		iLowH = 0;
-		iHighH = 50;
-		iLowS = 80;
-		iHighS = 255;
-		iLowV = 36;
-		iHighV = 255;
-		iErode = 15;
-		iUpperRange = 10;
 		isInitializedDepth = false;
 		isInitializedColor = false;
+		iErode = 15;
+		StepSize = 100;
 
 		cv::namedWindow(OPENCV_WINDOW);
-		cv::namedWindow(OPENCV_WINDOW_CONTROL);
 		cv::namedWindow(OPENCV_WINDOW_DEPTH);
-		//Create trackbars in "Control" window
-		cvCreateTrackbar("LowH", OPENCV_WINDOW_CONTROL.c_str(), &iLowH, 179); //Hue (0 - 179)
-		cvCreateTrackbar("HighH", OPENCV_WINDOW_CONTROL.c_str(), &iHighH, 179);
+		cv::namedWindow(OPENCV_WINDOW_CONTROL, CV_WINDOW_AUTOSIZE);
 
-		cvCreateTrackbar("LowS", OPENCV_WINDOW_CONTROL.c_str(), &iLowS, 255); //Saturation (0 - 255)
-		cvCreateTrackbar("HighS", OPENCV_WINDOW_CONTROL.c_str(), &iHighS, 255);
-
-		cvCreateTrackbar("LowV", OPENCV_WINDOW_CONTROL.c_str(), &iLowV, 255); //Value (0 - 255)
-		cvCreateTrackbar("HighV", OPENCV_WINDOW_CONTROL.c_str(), &iHighV, 255);
-		cvCreateTrackbar("Erode", OPENCV_WINDOW_CONTROL.c_str(), &iErode, 29); //Value (0 - 20)
-		cvCreateTrackbar("Distance", OPENCV_WINDOW_CONTROL.c_str(), &iUpperRange, 500); //Value (0 - 20)
+		cvCreateTrackbar("StepSize", OPENCV_WINDOW_CONTROL.c_str(), &StepSize, 400);
+		cvCreateTrackbar("Erode", OPENCV_WINDOW_CONTROL.c_str(), &iErode, 29);
 	}
 
 	~ObjectTracker()
 	{
-		cv::destroyWindow(OPENCV_WINDOW);
-		cv::destroyWindow(OPENCV_WINDOW_CONTROL);
+		cv::destroyWindow(OPENCV_WINDOW);;
 		cv::destroyWindow(OPENCV_WINDOW_DEPTH);
+		cv::destroyWindow(OPENCV_WINDOW_CONTROL);
 	}
 
 	void imageDepthCb(const sensor_msgs::ImageConstPtr& msgDepth)
@@ -106,6 +92,7 @@ public:
 	{
 		cv::Mat Depth;
 		Depth = cv_depth_ptr->image;
+		// Blur the image
 		GaussianBlur( Depth, Depth, Size(9, 9), 2, 2 );
 		*normalizedDepth = Depth;
 	}
@@ -122,7 +109,6 @@ public:
 		*grayScaleImage = tmpgrayScaleImage;
 	}
 
-
 	void getDirection()
 	{
 		if(isInitializedDepth == false) return;
@@ -134,34 +120,35 @@ public:
 		Mat mask;
 		// Create a normalized depth image, gaussian and mean filters used as well.
 		depthPreProcess(&normalizedDepth);
-		// Convert from BRG to HSV and use gaussian blur.
-		colorPreProcess(&HSVImage, &grayScaleImage);
-		// Only allow color in range that should be skin
+		HSVImage = cv_ptr->image;
 		
 		Mat threshDept;
-		Mat Blobs = Mat::zeros(HSVImage.size(), CV_8UC1);;
-		bool contourFound = false;
 		double occupancy;
 		int i = 10;
+
+		// Find the object that lies closest to the camera.
 		while (i < 3000)
 		{
-			inRange(normalizedDepth, i, i+100, threshDept);
+			inRange(normalizedDepth, i, i+StepSize, threshDept);
 			erode(threshDept, threshDept, getStructuringElement(MORPH_ELLIPSE, Size((iErode+1)/2, (iErode+1)/2)) );
 			occupancy = sum(threshDept)[0]/(640*480);
 			if(occupancy > 2){
-				cout << i << " - " << i+100 << endl;
+				cout << i << " - " << i+StepSize << endl;
 				break;	
 			} 
-			i = i+100;
+			i = i+StepSize;
 		}
+		
+		// Find the direction to the closes camera.
 		Moments m = moments(threshDept, false);
 		Point p1(m.m10/m.m00, m.m01/m.m00);
 		RNG rng(12345);
 		Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
 		circle( HSVImage, p1, 8, color, -1, 8, 0 );
 		
+		// Show the image
 		cv::imshow(OPENCV_WINDOW, HSVImage);
-		cv::imshow(OPENCV_WINDOW_GRAY, threshDept);
+		cv::imshow(OPENCV_WINDOW_DEPTH, threshDept);
 	}
 };
 
@@ -173,7 +160,7 @@ void handler(int) {
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "image_converter");
+	ros::init(argc, argv, "object_tracker");
 	ObjectTracker ic;
 	ros::Rate loop_rate(HZ);
 
